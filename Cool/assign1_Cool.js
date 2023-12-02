b4aa371dead417a5af402bdc7f230b1e42189549
Oliverthecrow <Olivermoser.jm@gmail.com>
2023-12-01 21:20:33 -0700

    changed variables to be sane and normal

--- Cool/assign1_Cool.js
+++ Cool/assign1_Cool.js
@@ -2,26 +2,34 @@ let score = 0;
 let name = "";
 let ranking = "";
 let scoreCheck = false;
-let Q1ANS = false;
-let Q2ANS = false;
-let Q3ANS = false;
-let Q4ANS = false;
-let Q5ANS = false;
+let Q1Answered = false;
+let Q2Answered = false;
+let Q3Answered = false;
+let Q4Answered = false;
+let Q5Answered = false;
 let testFinished = false;
-let Q1COR = false
-//Q1COR2 is intentional, there are two right answers to the first question, just one of them gives more points 
-let Q1COR2 = false
-let Q2COR = false
-let Q3COR = false
-let Q4COR = false
-let Q5COR = false
+let Q1Correct = false;
+//Q1MegaCorrect is intentional, there are two right answers to the first question, just one of them gives more points 
+let Q1MegaCorrect = false;
+let Q2Correct = false;
+let Q3Correct = false;
+let Q4Correct = false;
+let Q5Correct = false;
+let Q1 = "Question 1: Do you have own an air fryer? \n q) YES    w) NO    e) I HAVE TWENTY!";
+let Q2 = "Question 2: What animal out of the the choices is your favourite? \n r) Dogs    t) Cats    y) Birds";
+let Q3 = "Question 3: How many hours in Satisfactory do you have \n u) What is that?    i) >2     o) <2";
+let Q4 = "Question 4: Facourite subject in school \n p) Math     a) L.A     s) Social";
+let Q5 = "Question 5: What is 9 + 10 \n d) 19     f) 21     g) 90";
+let green = (50, 200, 50);
+let red = (200, 50, 50);
+let blue = (50, 50, 200);
 
 
 function setup() {
     let sketch = createCanvas(1920, 950);
     sketch.parent("mycanvas");
     name = window.prompt("What is your name?");
-    textFont('Courier New')
+    textFont('Courier New');
 }
 function preload() {
     BG = loadImage("./coolnessBG.jpg");
@@ -37,70 +45,70 @@ function draw() {
 
     strokeWeight(0);
     textSize(20);
-    if (Q1ANS === false) {
-        text("Question 1: Do you have own an air fryer? \n q) YES    w) NO    e) I HAVE TWENTY!", 50, 200);
+    if (!Q1Answered) {
+        text(Q1, 50, 200);
     }
-    else if (Q1COR === true) {
-        fill(50, 200, 50)
-        text("Question 1: Do you have own an air fryer? \n q) YES    w) NO    e) I HAVE TWENTY!", 50, 200);
-        fill(255)
+    else if (Q1Correct) {
+        fill(green);
+        text(Q1, 50, 200);
+        fill(255);
     }
-    else if (Q1COR2 === true) {
-        fill(50, 50, 200)
-        text("Question 1: Do you have own an air fryer? \n q) YES    w) NO    e) I HAVE TWENTY!", 50, 200);
-        fill(255)
+    else if (Q1MegaCorrect) {
+        fill(blue);
+        text(Q1, 50, 200);
+        fill(255);
     } else {
-        fill(200, 50, 50)
-        text("Question 1: Do you have own an air fryer? \n q) YES    w) NO    e) I HAVE TWENTY!", 50, 200);
-        fill(255)
+        fill(red);
+        text(Q1, 50, 200);
+        fill(255);
     }
-    if (Q2ANS === false) {
-        text("Question 2: What animal out of the the choices is your favourite? \n r) Dogs    t) Cats    y) Birds", 50, 300);
+    if (!Q2Answered) {
+        text(Q2, 50, 300);
     }
-    else if (Q2COR === true) {
-        fill(50, 200, 50)
-        text("Question 2: What animal out of the the choices is your favourite? \n r) Dogs    t) Cats    y) Birds", 50, 300);
-        fill(255)
+    else if (Q2Correct) {
+        fill(green);
+        text(Q2, 50, 300);
+        fill(255);
     } else {
-        fill(200, 50, 50)
-        text("Question 2: What animal out of the the choices is your favourite? \n r) Dogs    t) Cats    y) Birds", 50, 300);
-        fill(255)
+        fill(red);
+        text(Q2, 50, 300);
+        fill(255);
     }
-    if (Q3ANS === false) {
-        text("Question 3: How many hours in Satisfactory do you have \n u) What is that?    i) >2    o) <2", 50, 400);
+    if (!Q3Answered) {
+        text(Q3, 50, 400);
     }
-    else if (Q3COR === true) {
-        fill(50, 200, 50)
-        text("Question 3: How many hours in Satisfactory do you have \n u) What is that?    i) >2    o) <2", 50, 400);
-        fill(255)
+    else if (Q3Correct) {
+        fill(green);
+        text(Q3, 50, 400);
+        fill(255);
     } else {
-        fill(200, 50, 50)
-        text("Question 3: How many hours in Satisfactory do you have \n u) What is that?    i) >2    o) <2", 50, 400);
-        fill(255)
+        fill(red);
+        text(Q3, 50, 400);
+        fill(255);
     }
-    if (Q4ANS === false) {
-        text("Question 4: Favourite subject in school \n p) Math    a) L.A    s) Social", 50, 500);
+    if (!Q4Answered) {
+        text(Q4, 50, 500);
     }
-    else if (Q4COR === true) {
-        fill(50, 200, 50)
-        text("Question 4: Favourite subject in school \n p) Math    a) L.A    s) Social", 50, 500);
-        fill(255)
+    else if (Q4Correct) {
+        fill(green);
+        text(Q4, 50, 500);
+        fill(255);
     } else {
-        fill(200, 50, 50)
-        text("Question 4: Favourite subject in school \n p) Math    a) L.A    s) Social", 50, 500);
-        fill(255)
+        fill(red);
+        text(Q4, 50, 500);
+        fill(255);
     }
-    if (Q5ANS === false) {
-        text("Question 5: What is 9 + 10 \n d) 19    f) 21    g) 90", 50, 600);
+    if (!Q5Answered) {
+        text(Q5, 50, 600);
     }
-    else if (Q5COR === true) {
-        fill(50, 200, 50)
-        text("Question 5: What is 9 + 10 \n d) 19    f) 21    g) 90", 50, 600);
-        fill(255)
+    else if (!Q5Correct) {
+        fill(green);
+        text(Q5, 50, 600);
+        fill(255);
     } else {
-        fill(200, 50, 50)
-        text("Question 5: What is 9 + 10 \n d) 19    f) 21    g) 90", 50, 600);
-        fill(255)
+        fill(red);
+        text(Q5, 50, 600);
+        fill(255);
     }
 
     textSize(35)
@@ -123,83 +131,83 @@ function draw() {
 }
 function keyPressed() {
     // correct answers for test
-    if (key === "q" && Q1ANS === false) {
+    if (key === "q" && !Q1Answered) {
         score++;
-        Q1COR = true;
+        Q1Correct = true;
     }
-    else if (key === "e" && Q1ANS === false) {
+    else if (key === "e" && !Q1Answered) {
         score += 2;
-        Q1COR2 = true;
+        Q1MegaCorrect = true;
     }
-    else if (key === "t" && Q2ANS === false) {
+    else if (key === "t" && !Q2Answered) {
         score++;
-        Q2COR = true;
+        Q2Correct = true;
     }
-    else if (key === "o" && Q3ANS === false) {
+    else if (key === "i" && !Q3Answered) {
         score++;
-        Q3COR = true;
+        Q3Correct = true;
     }
-    else if (key === "p" && Q4ANS === false) {
+    else if (key === "p" && !Q4Answered) {
         score++;
-        Q4COR = true;
+        Q4Correct = true;
     }
 
-    else if (key === "f" && Q5ANS === false) {
-        score++
-        Q5COR = true;
+    else if (key === "f" && !Q5Answered) {
+        score++;
+        Q5Correct = true;
     }
     // to check test score
     if (key === "?") {
-        scoreCheck = true
+        scoreCheck = true;
         if (score === 6) {
-            ranking = "diamond"
+            ranking = "diamond";
         }
         else if (score === 5) {
-            ranking = "gold"
+            ranking = "gold";
         }
         else if (score >= 3) {
-            ranking = "silver"
+            ranking = "silver";
         }
         else {
-            ranking = "wood"
+            ranking = "wood";
         }
     }
-    else if (Q1ANS === true && Q2ANS === true && Q3ANS === true && Q4ANS === true && Q5ANS === true && scoreCheck === true) {
-        testFinished = true
+    else if (Q1Answered && Q2Answered && Q3Answered && Q4Answered  && Q5Answered  && scoreCheck) {
+        testFinished = true;
     }
     // resets variables to default
-    if (testFinished === true && key === "!") {
-        Q1ANS = false;
-        Q2ANS = false;
-        Q3ANS = false;
-        Q4ANS = false;
-        Q5ANS = false;
+    if (testFinished && key === "!") {
+        Q1Answered = false;
+        Q2Answered = false;
+        Q3Answered = false;
+        Q4Answered = false;
+        Q5Answered = false;
         scoreCheck = false;
         testFinished = false;
         score = 0;
         ranking = "";
-        Q1COR = false;
-        Q1COR2 = false;
-        Q2COR = false;
-        Q3COR = false;
-        Q4COR = false;
-        Q5COR = false;
+        Q1Correct = false;
+        Q1MegaCorrect = false;
+        Q2Correct = false;
+        Q3Correct = false;
+        Q4Correct = false;
+        Q5Correct = false;
 
     }
     //checks to see if a question has been asnwered
     if (key === "q" || key === "w" || key === "e") {
-        Q1ANS = true;
+        Q1Answered = true;
     }
     else if (key === "r" || key === "t" || key === "y") {
-        Q2ANS = true;
+        Q2Answered = true;
     }
     else if (key === "u" || key === "i" || key === "o") {
-        Q3ANS = true;
+        Q3Answered = true;
     }
     else if (key === "p" || key === "a" || key === "s") {
-        Q4ANS = true;
+        Q4Answered = true;
     }
     else if (key === "d" || key === "f" || key === "g") {
-        Q5ANS = true;
+        Q5Answered = true;
     }
 }
