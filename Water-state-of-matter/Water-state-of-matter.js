838170b6e539d5f37a635b9e36e19644d241c7a7
Oliverthecrow <Olivermoser.jm@gmail.com>
2023-12-04 21:08:21 -0700

    made a for loop cause I am learning, also I think the universe is speaking to me

--- Water-state-of-matter/Water-state-of-matter.js
+++ Water-state-of-matter/Water-state-of-matter.js
@@ -1,77 +1,41 @@
-
-let intervalId = setInterval(DrawWater, 1000);
-let intervalID2 = setInterval(BG, 10000);
-
 let WIW = window.innerWidth
 let WIH = window.innerHeight
-
-let x;
-let y;
 
 let water = {
     X: 0,
     Y: 0,
     R: 25,
-    color: "rgba(30, 70, 255, .7)",
 };
 function setup() {
     let sketch = createCanvas(WIW, WIH);
     sketch.parent("WaterSimulationCanvas");
 }
 function draw() { //--------------------------------- start of draw ---------------------------------------------------------//
-    background(0)
+    background(0);
     BG()
 
-    randomcords();
+    fill(30, 70, 255)
+    DrawWater();
 
-    fill(0);
-    stroke(255);
-    rect(100, 100, WIW - 200, window.innerHeight - 200);
-
-    clearInterval(intervalID)
-    clearInterval(intervalID2)
-
+    textAlign(CENTER);
+    textSize(25);
+    fill(255);
+    text("yes, this is all it does, it was practice to better understand functions and calling them.", WIW / 2, WIH - 30);
 }
 function mouseClicked() {
-    print(mouseY)
+    print(mouseY);
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 function DrawWater() {
-    fill(water.color)
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
-    circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
+    for (let circlecount = 0; circlecount < 30; circlecount++) {
+        circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
+        circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
+        circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
+        circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
+        circle(random(100 + water.R, WIW - 200 + water.R), random(100 + water.R, WIH - 150), water.R);
+    }
 }
 function BG() {
     fill(0);
